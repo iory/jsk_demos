c538745fd2fc9e82f5ecb447008596c933f5e223
iory <ab.ioryz@gmail.com>
2023-05-17 02:00:10 +0900

    Add training script

--- train/README.md
+++ train/README.md
@@ -1,27 +1,37 @@
 # train from object images.
 
-## prerequisite
+## prepare object images.
+
+Take an image of the object and create a separate directory for each name.
+Please refer to the directory `yamagata_items`.
 
 ```
-sudo apt update
-sudo apt install python3 python3-pip python3-venv
-sudo apt install -y python3-pyqt5
-sudo apt install -y qt5-default
-sudo apt install -y python-is-python3
-pip3 install -r requirements.txt --verbose
-python3 data.py
-```
+$ tree yamagata_items/
+yamagata_items/
+├── ba25
+│   ├── IMG_9596.HEIC.jpg
+│   ├── IMG_9597.HEIC.jpg
+│   └── IMG_9598.HEIC.jpg
+├── fan
+│   ├── IMG_9562.HEIC.jpg
+│   ├── IMG_9563.HEIC.jpg
+│   ├── IMG_9564.HEIC.jpg
+│   └── IMG_9565.HEIC.jpg
+└── rau
+    ├── IMG_9616.HEIC.jpg
+    ├── IMG_9617.HEIC.jpg
+    └── IMG_9618.HEIC.jpg
 
-Install cuda in addition to this.
-
-## remove background
-
-```
-python3 remove_bg.py yamagata_items
 ```
 
 ## train object detection model
 
+Run the train script. Specify the previous directory as an argument.
+
+Images are sent to a remote server and object detection training is performed.
+
 ```
-python3 generate_data.py --target-image-dir rembg_img/shootingstar-2023-05-08-14-13-45-148729-88999 -t fan rau ba25
+python train.py yamagata_items
 ```
+
+After training, a file containing the trained model (`best-%Y-%m-%d-%H-%M-%S-%f.pt`) and class name information (`from_images_dir-%Y-%m-%d-%H-%M-%S-%f.yaml`) will be copied.
---
+++ train/train.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import argparse
+import os.path as osp
+import subprocess
+import datetime
+
+
+def current_time_str(time_format='%Y-%m-%d-%H-%M-%S-%f'):
+    time_str = datetime.datetime.now().strftime(time_format)
+    return time_str
+
+
+def run_command(cmd, *args, **kwargs):
+    if kwargs.pop("capture_output", False):
+        kwargs["stdout"] = subprocess.PIPE
+        kwargs["stderr"] = subprocess.PIPE
+    return subprocess.run(cmd, *args, **kwargs)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Image training Script")
+    parser.add_argument('-u', '--username', type=str, default="thk")
+    parser.add_argument("--ip", type=str, default='133.11.216.13', help="IP Address")
+    parser.add_argument("image_directory", type=str, help="Image Directory")
+    args = parser.parse_args()
+
+    ssh_target = '{}@{}'.format(args.username, args.ip)
+    ssh_command = 'ssh {}'.format(ssh_target)
+    tmp_dir = osp.join('/tmp', 'project-t', '{}'.format(current_time_str()))
+    run_command('{} mkdir -p {}'.format(ssh_command, tmp_dir), shell=True)
+
+    source_image_dir = args.image_directory.rstrip('/')
+    rsync_image_command = 'rsync --verbose -r {} {}:{}'.format(source_image_dir, ssh_target, tmp_dir)
+    run_command(rsync_image_command, shell=True)
+
+    source_image_dir_in_remote = osp.join(tmp_dir, osp.basename(source_image_dir))
+
+    python_exe = 'python'
+    run_command(
+        '''{} 'bash --login -c "{} generate_data.py --from-images-dir {} -b 32"' '''.format(
+            ssh_command, python_exe, source_image_dir_in_remote),
+        shell=True)
+
+    date = current_time_str()
+    rsync_image_command = 'rsync --verbose {}:{} ./{}'.format(
+        ssh_target, '/tmp/from_images_dir/yolov7-seg-coco/weights/best.pt',
+        'best-{}.pt'.format(date))
+    run_command(rsync_image_command, shell=True)
+
+    rsync_image_command = 'rsync --verbose {}:{} ./{}'.format(
+        ssh_target, '/tmp/from_images_dir/from_images_dir.yaml',
+        'from_images_dir-{}.yaml'.format(date))
+    run_command(rsync_image_command, shell=True)
