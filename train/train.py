929c1ed97339a54889b0dabd355a2b203afcc68b
iory <ab.ioryz@gmail.com>
2023-05-17 03:30:31 +0900

    Use proxy command

--- train/train.py
+++ train/train.py
@@ -25,13 +25,18 @@ if __name__ == '__main__':
     parser.add_argument("image_directory", type=str, help="Image Directory")
     args = parser.parse_args()
 
+    proxy_command = "ssh -o ProxyCommand='ssh -W %h:%p {}@dlbox2.jsk.imi.i.u-tokyo.ac.jp'".format(args.username)
     ssh_target = '{}@{}'.format(args.username, args.ip)
-    ssh_command = 'ssh {}'.format(ssh_target)
+    ssh_command = "{} {}@{}".format(proxy_command, args.username, args.ip)
+
     tmp_dir = osp.join('/tmp', 'project-t', '{}'.format(current_time_str()))
-    run_command('{} mkdir -p {}'.format(ssh_command, tmp_dir), shell=True)
+    a = run_command('{} mkdir -p {}'.format(ssh_command, tmp_dir), shell=True)
 
     source_image_dir = args.image_directory.rstrip('/')
-    rsync_image_command = 'rsync --verbose -r {} {}:{}'.format(source_image_dir, ssh_target, tmp_dir)
+    rsync_image_command = 'rsync -e "{}" --verbose -r {} {}:{}'.format(
+        proxy_command,
+        source_image_dir, ssh_target, tmp_dir)
+    print(rsync_image_command)
     run_command(rsync_image_command, shell=True)
 
     source_image_dir_in_remote = osp.join(tmp_dir, osp.basename(source_image_dir))
@@ -43,12 +48,14 @@ if __name__ == '__main__':
         shell=True)
 
     date = current_time_str()
-    rsync_image_command = 'rsync --verbose {}:{} ./{}'.format(
+    rsync_image_command = 'rsync -e "{}" --verbose {}:{} ./{}'.format(
+        proxy_command,
         ssh_target, '/tmp/from_images_dir/yolov7-seg-coco/weights/best.pt',
         'best-{}.pt'.format(date))
     run_command(rsync_image_command, shell=True)
 
-    rsync_image_command = 'rsync --verbose {}:{} ./{}'.format(
+    rsync_image_command = 'rsync -e "{}" --verbose {}:{} ./{}'.format(
+        proxy_command,
         ssh_target, '/tmp/from_images_dir/from_images_dir.yaml',
         'from_images_dir-{}.yaml'.format(date))
     run_command(rsync_image_command, shell=True)
