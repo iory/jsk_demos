5a5e67e303e5e93049117f06782ad42acc283fb9
iory <ab.ioryz@gmail.com>
2023-05-16 17:56:07 +0900

    Fixed

--- train/generate_data.py
+++ train/generate_data.py
@@ -44,7 +44,7 @@ if __name__ == '__main__':
     subprocess.call('rm -rf /tmp/{}'.format(target),
                     shell=True)
     num = 20
-    jobs = min(multiprocessing.cpu_count() // 2, num)
+    jobs = min(multiprocessing.cpu_count(), num)
     sleep_time = 1.0
     verbose = False
     run_many(cmd, num, jobs=jobs,
