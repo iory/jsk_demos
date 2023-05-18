6f495480724f3314e1f15fafee467b4bb04bb6d6
iory <ab.ioryz@gmail.com>
2023-05-18 22:44:28 +0900

    Change owner

--- train/entrypoint.sh
+++ train/entrypoint.sh
@@ -11,6 +11,7 @@ if [ x"$USER_ID" != x"0" ]; then
     useradd -d /home/$USER_NAME -m -s /bin/bash -u $USER_ID -g $GROUP_ID $USER_NAME
 fi
 
+sudo chown -R $USER_NAME:$USER_NAME /home/$USER_NAME
 sudo chmod u-s /usr/sbin/useradd
 sudo chmod u-s /usr/sbin/groupadd
 
