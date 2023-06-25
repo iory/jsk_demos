9488c1bea353e74d0d6b49f4d81595e4cb215d29
iory <ab.ioryz@gmail.com>
2023-06-26 04:21:52 +0900

    Add top n color debug image

--- train/top_n_color.py
+++ train/top_n_color.py
@@ -44,3 +44,33 @@ def take_random_n_color(path, n=10):
         save_json(colors, path.with_suffix('.json'))
     color = random.choice(colors)
     return color
+
+
+if __name__ == '__main__':
+    from PIL import Image
+    IMG_SIZE = 64
+    MARGIN = 0
+    width = IMG_SIZE * 3 + MARGIN * 2
+    height = IMG_SIZE + MARGIN * 2
+
+    from eos import make_fancy_output_dir
+    from eos import makedirs
+    output_path = Path(make_fancy_output_dir('./results', no_save=True))
+    for path in img_paths:
+        tiled_color_img = Image.new(
+            mode='RGB', size=(width, height), color='#333333')
+        pil_img, pil_mask = cached_imread(path)
+        n = 3
+        c = n + 1
+        colors = list(top_n_colors(pil_img, top_n=n, num_of_clusters=c, mask=pil_mask))
+
+        for i, rgb_arr in enumerate(colors):
+            color_hex_str = '#%02x%02x%02x' % tuple(rgb_arr)
+            color_img = Image.new(
+                mode='RGB', size=(IMG_SIZE, IMG_SIZE),
+                color=color_hex_str)
+            tiled_color_img.paste(
+                im=color_img,
+                box=(MARGIN + IMG_SIZE * i, MARGIN))
+        makedirs(output_path / path.parent.name)
+        tiled_color_img.save(output_path / path.parent.name / path.name)
